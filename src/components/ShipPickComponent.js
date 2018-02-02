d75de169bee8a22d7038319e655666f00033e534
jhta26 <hsujasonf@gmail.com>
2018-02-02 12:40:15 -0800

    working on picking a ship component

--- src/components/ShipPickComponent.js
+++ src/components/ShipPickComponent.js
@@ -2,14 +2,21 @@ import React, {Component} from 'react'
 
 export default class ShipPickComponent extends Component{
 	state={
-
-		
+		shipName:'',
+		shipSize:0,
+		vertical: true
 	}
 
 	render(){
 		return(
 
+<div className="shipPick">
 
+
+
+
+
+</div>
 
 
 
