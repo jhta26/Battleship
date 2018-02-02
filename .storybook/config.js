e8420c1149a2886771e824949365803636365126
jhta26 <hsujasonf@gmail.com>
2018-02-02 12:43:41 -0800

    installed storybook

---
+++ .storybook/config.js
@@ -0,0 +1,19 @@
+import { configure } from '@storybook/react';
+function loadStories() {
+  // Require your stories here...
+}
+configure(loadStories, module);
+
+
+
+// import { configure } from '@storybook/react';
+// function loadStories() {
+//   require('../src/index.css');
+//   require('../src/components/MessageComponent.story');
+//   require('../src/components/ToolbarComponent.story');
+//   require('../src/components/MessagesComponent.story');
+//   require('../src/components/ComposeFormComponent.story');
+//   require('../src/components/InboxPageLayout.story');
+// }
+// configure(loadStories, module);
+
